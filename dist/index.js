04f12169e05cab03b6ae9d8a42c75af95d532e29
Vincent Wong <vincentwong5609@gmail.com>
2023-07-09 00:54:19 -0700

    finished main card

--- dist/index.js
+++ dist/index.js
@@ -8,17 +8,32 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+const locationInput = document.getElementById('location');
+const searchButton = document.getElementById('search-button');
+const cityName = document.getElementById('city-name');
+const temp = document.getElementById('temp');
+const condition = document.getElementById('condition');
 const apiKey = "c95875ed54ca4976bba11711232206";
-const apiUrl = "http://api.weatherapi.com/v1/current.json?aqi=no&q=Vancouver";
-const checkWeather = () => __awaiter(void 0, void 0, void 0, function* () {
+const apiUrl = "http://api.weatherapi.com/v1/current.json?aqi=no&q=";
+const checkWeather = (location) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const res = yield fetch(apiUrl + `&key=${apiKey}`);
+        const res = yield fetch(apiUrl + `${location}&key=${apiKey}`);
         console.log(res.ok);
         const data = yield res.json();
-        console.log(data);
+        return data;
     }
     catch (err) {
         console.error(err);
     }
 });
-checkWeather();
+const changeInfo = (data) => {
+    cityName.innerHTML = data.location.name;
+    temp.innerHTML = `${data.current.temp_c}°<sup id="celcius">C</sup>`;
+    condition.innerHTML = data.current.condition.text;
+};
+searchButton === null || searchButton === void 0 ? void 0 : searchButton.addEventListener('click', () => {
+    checkWeather(locationInput.value)
+        .then((res) => {
+        changeInfo(res);
+    });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,39 @@
-const apiKey: string = "c95875ed54ca4976bba11711232206";
-const apiUrl: string = "http://api.weatherapi.com/v1/current.json?aqi=no&q=Vancouver";
+const locationInput = document.getElementById('location') as HTMLInputElement;
+const searchButton = document.getElementById('search-button');
+const cityName = document.getElementById('city-name') as HTMLElement;
+const temp = document.getElementById('temp') as HTMLElement;
+const condition = document.getElementById('condition') as HTMLElement;
 
-const checkWeather = async () => {
+const apiKey: string = "c95875ed54ca4976bba11711232206"; // declares api key variable
+const apiUrl: string = "http://api.weatherapi.com/v1/current.json?aqi=no&q="; // declares api url
+
+const checkWeather = async (location: string) => {
     try {
-        const res = await fetch(apiUrl + `&key=${apiKey}`); 
+        const res = await fetch(apiUrl + `${location}&key=${apiKey}`); 
         console.log(res.ok)
-        const data= await res.json()
-        console.log(data)
+        const data = await res.json()
+        return data
 
     }catch(err) {
         console.error(err)
     }
 };
 
-checkWeather()
+const changeInfo = (data: any) => {
+    cityName.innerHTML = `<p id="city-name">${data.location.name}</p>`
+    temp.innerHTML = `${data.current.temp_c}°<sup id="celcius">C</sup>`
+    condition.innerHTML = data.current.condition.text
+}
+
+searchButton?.addEventListener('click', () => {
+    checkWeather(locationInput.value)
+    .then((res) => {
+        changeInfo(res)
+    })
+})
+
+
+
+
+
+
